9dd1b7b2824b2cf0705503eee78092f20be28aeb
laierin615 <laierin615@gmail.com>
2026-08-18 12:10:32 +0800

    Deploy Einstein campus quest

---
+++ backend-config.js
@@ -0,0 +1,5 @@
+// 部署 Google Apps Script 後，將 scriptUrl 改為網頁應用程式網址。
+// 教師密碼請只設定在 Apps Script 的「指令碼屬性」ADMIN_PASSWORD，切勿寫在這裡。
+window.EINSTEIN_BACKEND = {
+  scriptUrl: 'PASTE_GOOGLE_APPS_SCRIPT_WEB_APP_URL_HERE'
+};
